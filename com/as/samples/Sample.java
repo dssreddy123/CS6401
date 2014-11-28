723c6d2f945e4f54f70b802de7846551c6d944a4
DESAM SYAMA SUNDARA REDDY <dssreddy123@gmail.com>
2014-11-28 06:38:54 +0530

    Update Sample.java

--- com/as/samples/Sample.java
+++ com/as/samples/Sample.java
@@ -2,7 +2,7 @@ package com.as.samples;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
+//import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
