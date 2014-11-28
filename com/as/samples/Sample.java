8520620eae3a16c5b4cd5ec08e815d4795204386
dssreddy123 <dssreddy123@gmail.com>
2014-11-28 07:16:01 +0530

    syam4

--- com/as/samples/Sample.java
+++ com/as/samples/Sample.java
@@ -3,9 +3,9 @@ package com.as.samples;
 import java.io.IOException;
 
 //import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
 
 public class Sample  extends HttpServlet{
 
